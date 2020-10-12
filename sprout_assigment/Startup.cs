16230d6697a32906e46f30e764225aecde88121b
Oleg Rukin <orukin@gmail.com>
2020-10-12 11:30:04 +0300

    enum serialization

--- sprout_assigment/Startup.cs
+++ sprout_assigment/Startup.cs
@@ -12,7 +12,8 @@ namespace sprout_assigment
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers()
+                    .AddNewtonsoftJson();
             services.AddApiVersioning(o => {
                 o.ReportApiVersions = true;
                 o.AssumeDefaultVersionWhenUnspecified = true;
